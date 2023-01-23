fa4c1b8f696c8252f4c9fed502dd0c790a48df9e
Davi Araki <davi.araki@gmail.com>
2023-01-23 10:50:45 -0300

    Creating a design-system

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Testando o elemento Text',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    as: 'strong',
+  },
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  theme,
+  keyframes,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
+  },
+})
