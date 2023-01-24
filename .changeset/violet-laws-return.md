6cd6398192845b29fa2cec8346077be1ff622821
Davi Araki <davi.araki@gmail.com>
2023-01-24 13:18:43 -0300

    add changeset

---
+++ .changeset/violet-laws-return.md
@@ -0,0 +1,8 @@
+---
+"@daviaraki-ui/eslint-config": major
+"@daviaraki-ui/ts-config": major
+"@daviaraki-ui/tokens": major
+"@daviaraki-ui/react": major
+---
+
+Create package
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@daviaraki-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
